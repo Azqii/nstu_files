32d088e48d7f73443b9f8c70531d5327d9982851
Azqii <109161021+Azqii@users.noreply.github.com>
2024-01-11 13:42:35 +0700

    brackets

--- cpp/algs_kr_1/Tree.h
+++ cpp/algs_kr_1/Tree.h
@@ -7,11 +7,9 @@
 
 
 template <class K, class V>
-class Tree
-{
+class Tree {
 protected:
-    class Node
-    {
+    class Node {
     public:
         K key;
         V value;
@@ -28,21 +26,20 @@ protected:
     Node* root;
 
 public:
-    class Iterator
-    {
+    class Iterator {
     private:
         Tree* ptr;
         Node* cur;
 
     public:
-        Iterator(Tree *);  // TODO
-        K& operator *();  // TODO
-        Iterator &operator ++();  // TODO
-        bool operator ==(const Iterator&);  // TODO
+        Iterator(Tree*); // TODO
+        K& operator *(); // TODO
+        Iterator& operator ++(); // TODO
+        bool operator ==(const Iterator&); // TODO
     };
 
-    Iterator begin();  // TODO
-    Iterator end();  // TODO
+    Iterator begin(); // TODO
+    Iterator end(); // TODO
 
     Tree();
     int getSize() const;
@@ -53,13 +50,12 @@ public:
     void remove(K key);
     void printKeysByScheme() const;
     void printStructure() const;
-    K searchBySerialNumber(int serialNumber) const;  // TODO
+    K searchBySerialNumber(int serialNumber) const; // TODO
 };
 
 
 template <class K, class V>
-Tree<K, V>::Node::Node(K key, V value)
-{
+Tree<K, V>::Node::Node(K key, V value) {
     this->key = key;
     this->value = value;
     this->left = nullptr;
@@ -67,17 +63,14 @@ Tree<K, V>::Node::Node(K key, V value)
 }
 
 template <class K, class V>
-Tree<K, V>::Tree()
-{
+Tree<K, V>::Tree() {
     this->root = nullptr;
 }
 
 template <class K, class V>
-int Tree<K, V>::countAllSubNodes(Node* node) const
-{
+int Tree<K, V>::countAllSubNodes(Node* node) const {
     int counter = 0;
-    if (node == nullptr)
-    {
+    if (node == nullptr) {
         return counter;
     }
     counter += this->countAllSubNodes(node->left);
@@ -87,55 +80,44 @@ int Tree<K, V>::countAllSubNodes(Node* node) const
 }
 
 template <class K, class V>
-int Tree<K, V>::getSize() const
-{
+int Tree<K, V>::getSize() const {
     return this->countAllSubNodes(this->root);
 }
 
 template <class K, class V>
-void Tree<K, V>::insert(K key, V value)
-{
-    if (this->root == nullptr)
-    {
+void Tree<K, V>::insert(K key, V value) {
+    if (this->root == nullptr) {
         this->root = new Node(key, value);
         return;
     }
 
     Node* node = this->root;
     Node* pred = nullptr;
-    while (node != nullptr)
-    {
+    while (node != nullptr) {
         pred = node;
-        if (key == node->key)
-        {
+        if (key == node->key) {
             node->value = value;
             return;
         }
 
-        if (key < node->key)
-        {
+        if (key < node->key) {
             node = node->left;
         }
-        else
-        {
+        else {
             node = node->right;
         }
     }
-    if (key < pred->key)
-    {
+    if (key < pred->key) {
         pred->left = new Node(key, value);
     }
-    else
-    {
+    else {
         pred->right = new Node(key, value);
     }
 }
 
 template <class K, class V>
-void Tree<K, V>::removeAllSubNodes(Node*& node)
-{
-    if (node == nullptr)
-    {
+void Tree<K, V>::removeAllSubNodes(Node*& node) {
+    if (node == nullptr) {
         return;
     }
     this->removeAllSubNodes(node->left);
@@ -145,101 +127,80 @@ void Tree<K, V>::removeAllSubNodes(Node*& node)
 }
 
 template <class K, class V>
-void Tree<K, V>::clear()
-{
+void Tree<K, V>::clear() {
     this->removeAllSubNodes(this->root);
 }
 
 template <class K, class V>
-bool Tree<K, V>::isEmtpy() const
-{
+bool Tree<K, V>::isEmtpy() const {
     return this->root == nullptr;
 }
 
 template <class K, class V>
-V Tree<K, V>::search(K key) const
-{
+V Tree<K, V>::search(K key) const {
     Node* node = this->root;
-    while (node != nullptr && key != node->key)
-    {
-        if (key < node->key)
-        {
+    while (node != nullptr && key != node->key) {
+        if (key < node->key) {
             node = node->left;
         }
-        else
-        {
+        else {
             node = node->right;
         }
     }
-    if (node == nullptr)
-    {
+    if (node == nullptr) {
         throw std::invalid_argument("There's no such node with the given key");
     }
     return node->value;
 }
 
 template <class K, class V>
-void Tree<K, V>::remove(K key)
-{
+void Tree<K, V>::remove(K key) {
     Node* node = this->root;
     Node* pred = nullptr;
-    while (node != nullptr && node->key != key)
-    {
+    while (node != nullptr && node->key != key) {
         pred = node;
-        if (key < node->key)
-        {
+        if (key < node->key) {
             node = node->left;
         }
-        else
-        {
+        else {
             node = node->right;
         }
     }
 
-    if (node == nullptr)
-    {
+    if (node == nullptr) {
         return;
     }
 
-    if (node->left == nullptr || node->right == nullptr)
-    {
+    if (node->left == nullptr || node->right == nullptr) {
         Node* newNode = nullptr;
-        if (node->left == nullptr)
-        {
+        if (node->left == nullptr) {
             newNode = node->right;
         }
-        else
-        {
+        else {
             newNode = node->left;
         }
 
-        if (node == pred->left)
-        {
+        if (node == pred->left) {
             pred->left = newNode;
         }
-        else
-        {
+        else {
             pred->right = newNode;
         }
 
         delete node;
     }
-    else
-    {
+    else {
         Node* parent = nullptr;
         Node* temp = node->right;
 
-        while (temp->left != nullptr)
-        {
+        while (temp->left != nullptr) {
             parent = temp;
             temp = temp->left;
         }
-        if (parent != nullptr)
-        {
+        if (parent != nullptr) {
             parent->left = temp->right;
         }
-        else
-        {
+        else {
             node->right = temp->right;
         }
 
@@ -249,10 +210,8 @@ void Tree<K, V>::remove(K key)
 }
 
 template <class K, class V>
-void Tree<K, V>::printAllKeysByScheme(Node* node) const
-{
-    if (node == nullptr)
-    {
+void Tree<K, V>::printAllKeysByScheme(Node* node) const {
+    if (node == nullptr) {
         return;
     }
     this->printAllKeysByScheme(node->left);
@@ -262,22 +221,18 @@ void Tree<K, V>::printAllKeysByScheme(Node* node) const
 
 
 template <class K, class V>
-void Tree<K, V>::printKeysByScheme() const
-{
+void Tree<K, V>::printKeysByScheme() const {
     this->printAllKeysByScheme(this->root);
     std::cout << std::endl;
 }
 
 template <class K, class V>
-void Tree<K, V>::showKeyWithIndent(Node* node, int level) const
-{
-    if (node == nullptr)
-    {
+void Tree<K, V>::showKeyWithIndent(Node* node, int level) const {
+    if (node == nullptr) {
         return;
     }
     this->showKeyWithIndent(node->right, level + 1);
-    for (int i = 0; i < 3 * level; i++)
-    {
+    for (int i = 0; i < 3 * level; i++) {
         std::cout << " ";
     }
     std::cout << node->key << std::endl;
@@ -286,8 +241,7 @@ void Tree<K, V>::showKeyWithIndent(Node* node, int level) const
 
 
 template <class K, class V>
-void Tree<K, V>::printStructure() const
-{
+void Tree<K, V>::printStructure() const {
     this->showKeyWithIndent(this->root, 0);
     std::cout << std::endl;
 }
